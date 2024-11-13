0384a87b0e185bd0499c9c333ed72fc5ee72dd5b
Liliana Vera <lverag28@gmail.com>
2024-11-13 15:53:58 -0500

    controlador apciente o

---
+++ src/main/java/com/example/ServidorSura5/SERVICIOS/ServicioPaciente.java
@@ -0,0 +1,29 @@
+package com.example.ServidorSura5.SERVICIOS;
+
+import com.example.ServidorSura5.MODELOS.Paciente;
+import com.example.ServidorSura5.REPOSITORIOS.IRepositorioPaciente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServicioPaciente {
+
+    //1. LLamar al repositorio respectivo
+    @Autowired
+    IRepositorioPaciente iRepositorioPaciente;
+
+    //2. Se programan las funciones para las distintas
+    //operaciones en la BD
+
+    //Funcion para guardar DATOS
+    public Paciente guardarPaciente(Paciente datosPaciente)throws Exception{
+        try{
+            return iRepositorioPaciente.save(datosPaciente);
+
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+
+}
