cd16e092c39ac1a5616182a9c4ca3406712e481a
Liliana Vera <lverag28@gmail.com>
2024-11-20 16:11:34 -0500

    se crean pruebas exitosas

--- src/main/java/com/example/ServidorSura5/SERVICIOS/ServicioPaciente.java
+++ src/main/java/com/example/ServidorSura5/SERVICIOS/ServicioPaciente.java
@@ -5,6 +5,8 @@ import com.example.ServidorSura5.REPOSITORIOS.IRepositorioPaciente;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ServicioPaciente {
 
@@ -20,6 +22,14 @@ public class ServicioPaciente {
         try{
             return iRepositorioPaciente.save(datosPaciente);
 
+        }catch(Exception error){
+            throw new Exception(error.getMessage());
+        }
+    }
+
+    public List<Paciente> buscarPacientes()throws Exception{
+        try{
+            return iRepositorioPaciente.findAll();
         }catch(Exception error){
             throw new Exception(error.getMessage());
         }
